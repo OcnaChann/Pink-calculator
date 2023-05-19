1368fc3889f8685d888a205378f865cc25bdb6d1
OcnaChann <ocansherchan@gmail.com>
2023-05-19 11:25:27 +0545

    First Commit

---
+++ app/src/main/java/com/example/adcalculator/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.calculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnAC.setOnClickListener {
+            binding.tVInput.text = ""
+            binding.tVOutput.text = ""
+        }
+        binding.btnCancel.setOnClickListener {
+            val currentInput = binding.tVInput.text.toString()
+            if (currentInput.isNotEmpty()) {
+                binding.tVInput.text = currentInput.substring(0, currentInput.length - 1)
+            }
+        }
+        binding.btnEqual.setOnClickListener {
+            val input = binding.tVInput.text.toString()
+            val result = evaluateExpression(input)
+            binding.tVOutput.text = result.toString()
+        }
+
+        binding.btn1.setOnClickListener { appendToInput("1") }
+        binding.btn2.setOnClickListener { appendToInput("2") }
+        binding.btn3.setOnClickListener { appendToInput("3") }
+        binding.btn4.setOnClickListener { appendToInput("4") }
+        binding.btn5.setOnClickListener { appendToInput("5") }
+        binding.btn6.setOnClickListener { appendToInput("6") }
+        binding.btn7.setOnClickListener { appendToInput("7") }
+        binding.btn8.setOnClickListener { appendToInput("8") }
+        binding.btn9.setOnClickListener { appendToInput("9") }
+        binding.btn0.setOnClickListener { appendToInput("0") }
+        binding.btnDot.setOnClickListener { appendToInput(".") }
+        binding.btnPlus.setOnClickListener { appendToInput("+") }
+        binding.btnMinus.setOnClickListener { appendToInput("-") }
+        binding.btnMultiply.setOnClickListener { appendToInput("*") }
+        binding.btnDivide.setOnClickListener { appendToInput("/") }
+    }
+
+    private fun appendToInput(str: String) {
+        val currentInput = binding.tVInput.text.toString()
+        val newInput = currentInput + str
+        binding.tVInput.text = newInput
+    }
+
+    private fun evaluateExpression(input: String): Double {
+        var result = 0.0
+
+        // Split the input string into individual numbers and operators
+        val numbers = input.split("[+\\-*/]".toRegex())
+        val operators = input.split("\\d+(\\.\\d+)?".toRegex()).dropLastWhile { it.isEmpty() }
+
+        // Perform the arithmetic calculations based on the operators
+        if (numbers.isNotEmpty()) {
+            result = numbers[0].toDouble()
+            for (i in 1 until numbers.size) {
+                when (operators[i]) {
+                    "+" -> result += numbers[i].toDouble()
+                    "-" -> result -= numbers[i].toDouble()
+                    "*" -> result *= numbers[i].toDouble()
+                    "/" -> result /= numbers[i].toDouble()
+                }
+            }
+        }
+
+        return result
+    }
+}
